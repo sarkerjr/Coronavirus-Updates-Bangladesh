4dacd359973d01e7d1024a0b10300dc771765741
Asif Shahriar <a.shahriar1999@gmail.com>
2020-04-29 02:35:10 +0600

    Added HyperLinks

--- app/src/main/java/com/sarkerjr/widgetpractice/MainActivity.java
+++ app/src/main/java/com/sarkerjr/widgetpractice/MainActivity.java
@@ -3,7 +3,11 @@ package com.sarkerjr.widgetpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +21,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+         findViewById(R.id.app).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://github.com/sarkerjr/Coronavirus-Updates-Bangladesh"));
+                startActivity(i);
+            }
+});
+
+        findViewById(R.id.api).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://github.com/asifo1/corona_bd_api"));
+                startActivity(i);
+            }
+        });
+        findViewById(R.id.anik).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://github.com/sarkerjr"));
+                startActivity(i);
+            }
+        });
+        findViewById(R.id.asif).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://asifo1.github.io"));
+                startActivity(i);
+            }
+        });
     }
 }
